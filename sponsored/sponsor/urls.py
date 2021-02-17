80206bb58e72427494d60682cd0f95abcca96645
HarshKakran <harshkakran05@gmail.com>
2021-02-17 13:36:19 +0530

    sponsor almost complete

--- sponsored/sponsor/urls.py
+++ sponsored/sponsor/urls.py
@@ -4,5 +4,8 @@ from . import views
 app_name = 'sponsor'
 
 urlpatterns = [
-    # path('', )
+    path('event/<int:pk>', views.EventDetails.as_view(), name='event_detail'),
+    path('add-sponsor/<int:pk>', views.add_sponsor, name='add_sponsor'),
+    path('<slug:slug>', views.NearByEvents.as_view(), name='homepage'),
+    path('', views.CityList.as_view(), name='city_list')
 ]
