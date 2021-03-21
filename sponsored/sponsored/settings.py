38873ee90e5c9aa47154f64e76d420725dce1bd6
HarshKakran <harshkakran05@gmail.com>
2021-03-21 23:12:05 +0530

    Prepared for Heroku Deployment

--- sponsored/sponsored/settings.py
+++ sponsored/sponsored/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '03a404j1+et1-gryekza=99%@75(e$-2023n(g69+am7_m$l)k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://sponsored-app.herokuapp.com/']
 
 
 # Application definition
